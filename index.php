5a6b3bf960eaa2df441d21445a7dbada71543820
Shaikh-Sadi <sm.shaikhsadi@gmail.com>
2018-01-09 16:38:22 +0600

    edited

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 <head>
 	<link rel="stylesheet" href="style.css" type="text/css"/>
 	
-	<!--link rel="stylesheet" href="css/bootstrap.css" /-->
+	
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
-	<!--link rel="stylesheet" href="css/bootstrap-grid.css" />
-	<link rel="stylesheet" href="css/bootstrap-grid.min.css" />
-	<link rel="stylesheet" href="css/bootstrap-reboot.css" />
-	<link rel="stylesheet" href="css/bootstrap-reboot.min.css" /-->
+	
 	
 	
 	
